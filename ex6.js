1d8ae28be9da74a1ac0b1d5d98b869d34265245c
Heloisa31 <heloisa.souza.pinto@escola.pr.gov.br>
2025-02-17 08:12:17 -0300

    Add files via upload

---
+++ ex6.js
@@ -0,0 +1,7 @@
+const largura = 4
+const altura = 5
+
+const calculo = largura*altura
+
+console.log (calculo)
+
---
+++ ex7.js
@@ -0,0 +1,5 @@
+const celsius = 6
+
+const conversao = (celsius*9/5 )+32
+
+console.log (conversao)
